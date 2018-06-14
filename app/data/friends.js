33e8d71dfa7d9af8a6cc1d290d0ae348859da466
Eric Baltutis <baltu008@umn.edu>
2018-06-14 02:57:22 -0500

    add friends on friends.js, finish layout, & fix links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,95 @@
-var friendsArray = [
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var surveyArray = [
     {
-        friendName: "Ahmed",
-        friendPhoto: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        friendScores: [
+        "name": "Leslie Nielsen",
+        "photo": "https://media.npr.org/assets/img/2010/11/29/ap101128059850_custom-c3b9829b1828124f7f31620b834e8f17015ddc95-s300-c85.jpg",
+        "scores": [
             5,
-            1,
-            4,
-            4,
             5,
+            4,
             1,
+            1,
+            1,
+            5,
             2,
-            5,
-            4,
+            1,
             1
         ]
+    },
+    {
+        "name": "John Travolta",
+        "photo": "https://pmcdeadline2.files.wordpress.com/2017/05/ap_17009385805053.jpg?w=605",
+        "scores": [
+            5,
+            5,
+            1,
+            5,
+            3,
+            5,
+            4,
+            4,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "A Normal Person",
+        "photo": "https://i.imgflip.com/zrjrm.jpg",
+        "scores": [
+            5,
+            4,
+            5,
+            5,
+            4,
+            5,
+            5,
+            3,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "John Goodman",
+        "photo": "https://uproxx.files.wordpress.com/2015/05/w-sob-2.jpg?quality=95&w=650",
+        "scores": [
+            1,
+            5,
+            5,
+            5,
+            5,
+            1,
+            5,
+            5,
+            5,
+            1
+        ]
+    },
+    {
+        "name": "Will Ferrell",
+        "photo": "https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzc3ODM1MDIy/will-ferrell-9542601-1-402.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
     }
-]
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = surveyArray;
+
+
+
--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
-    console.log("App is listening on PORT: " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
